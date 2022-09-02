8f00c864ec20d2e109940fcb0988b46436369745
Yusuke Sakai <sakai.yusuke@classmethod.jp>
2022-09-02 16:28:02 +0900

    feat: UI 上に比較結果を表示

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,12 @@
       CloudFront + S3 から取得したリビジョンID:
       <span style="color: blue">{{ serverRevision }}</span>
     </p>
+    <p>
+      リビジョンIDを比較:
+      <span v-if="appRevisionId === serverRevision" style="color: blue"
+        >同じです</span
+      ><span v-else style="color: red">異なります！！</span>
+    </p>
   </div>
 </template>
 
@@ -17,34 +23,38 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  data: () => {
+  data(): any {
     return {
       serverRevision: '',
+      intervalId: 0,
     }
   },
   computed: {
     appRevisionId(): string {
       return process.env.APP_REVISION_ID ?? ''
     },
-    env():any {
-      return ;
-    }
   },
-  async asyncData({ app }) {
-    console.log("env: ", process.env)
-    const res = await fetch(
-      `${process.env.REVISION_URL}/application/revision.json`
-    )
-    try {
-      const revision = await res.json()
-      return {
-        serverRevision: revision.revision ?? '',
+  methods: {
+    async getRevisionId(): Promise<void> {
+      try {
+        const res = await fetch(
+          `${process.env.REVISION_URL}/application/revision.json`
+        )
+        const revision = await res.json()
+        this.serverRevision = revision?.revision ?? ''
+      } catch (error) {
+        console.error(error)
       }
-    } catch (error) {
-      return {
-        serverRevision: '',
-      }
-    }
+    },
+  },
+  mounted() {
+    this.getRevisionId()
+    this.intervalId = window.setInterval(() => {
+      this.getRevisionId()
+    }, 5 * 1000) // 5秒に1回ポーリング
+  },
+  beforeDestroy() {
+    window.clearInterval(this.intervalId)
   },
 })
 </script>
